7f6e66ed89bfe9b4ae771a2044dc4b5b6678059a
Oskandaria <oskandaria@gmail.com>
2024-04-30 07:49:50 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
+Route::get('/dashboard12', function () {
     return view('pages.dashboard',['type_menu'=>'dashboard']);
 });
 
